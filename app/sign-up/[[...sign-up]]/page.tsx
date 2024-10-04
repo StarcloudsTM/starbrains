3897ebad99ad3555bcdf69162e334075107cb483
StarcloudsTM <teamlnhub@gmail.com>
2024-10-04 15:08:25 +0530

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Link from "next/link";
+
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSignUp } from "@clerk/nextjs";
